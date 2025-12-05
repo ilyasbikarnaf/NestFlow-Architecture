d0739eb82a56a20356319ce6672f25d0a4ef081f
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-12-05 09:14:51 +0100

    feat(auth): initialize auth module structure
    - Create auth module foundation
    - Prepare structure for authentication strategies and services

---
+++ src/auth/providers/auth.service.ts
@@ -0,0 +1,19 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+
+  public login(email: string, password: string, id: string) {
+    const user = this.usersService.findUserById(id);
+    return 'SAMPLE_TOKEN';
+  }
+
+  isAuth() {
+    return true;
+  }
+}
