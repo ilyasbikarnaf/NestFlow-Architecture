660c75c7442be81395d35aa3642c806d67efa92a
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-12-05 09:14:51 +0100

    refactor(app): integrate all modules and update app configuration
    - Import UsersModule, PostsModule, TagsModule, AuthModule, MetaOptionsModule
    - Configure root application module
    - Update app controller with new routing structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin',
+        database: 'nestjs-blog',
+
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    TagsModule,
+    MetaOptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
