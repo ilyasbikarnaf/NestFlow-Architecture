29d6e3e0d0822f5ee8ed0baa2b4af4643c3eaaaa
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-12-17 11:52:02 +0100

    feat(auth): add active user decorator and interface

---
+++ src/auth/decorators/active-user.decorator.ts
@@ -0,0 +1,17 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
+import { REQUEST_USER_KEY } from '../constants/auth.constants';
+
+export const ActiveUser = createParamDecorator(
+  (field: keyof ActiveUserData | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+
+    const user: ActiveUserData = request[REQUEST_USER_KEY];
+
+    if (field) {
+      return user?.[field];
+    }
+
+    return user;
+  },
+);
